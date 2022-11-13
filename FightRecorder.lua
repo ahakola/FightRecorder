88f1ffff3126b144f37c03ac554da0917666bc37
Unknown <40722602+ahakola@users.noreply.github.com>
2022-11-14 01:21:45 +0200

    Fix missing comma in the buffs list

--- FightRecorder.lua
+++ FightRecorder.lua
@@ -160,7 +160,7 @@ local buffsTable = {
 	"Time Warp", -- Mage - 80353
 	"Ancient Hysteria", -- Core Hound (Hunter pet) - 90355
 	"Netherwinds", -- Nether Ray (Hunter pet) - 160452
-	"Drums of Battle" -- TBC - 35476 (https://www.wowhead.com/item=29529/drums-of-battle#comments)
+	"Drums of Battle", -- TBC - 35476 (https://www.wowhead.com/item=29529/drums-of-battle#comments)
 	"Drums of Rage", -- MoP - 146555 (http://www.wowhead.com/item=102351/drums-of-rage#comments:id=1912641)
 	"Drums of Fury", -- WoD - 178207 (https://www.wowhead.com/item=120257/drums-of-fury#comments)
 	"Drums of the Mountain", -- Legion - 230935 (https://www.wowhead.com/item=142406/drums-of-the-mountain#comments)
